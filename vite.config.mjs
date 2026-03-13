df1a818619b669c71007363affb9450229006983
hospyhematology-spec <hospy.hematology@gmail.com>
2026-03-13 17:14:22 +0900

    Fix: use relative base path for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/regimen_table_maker/',
+  // GitHub Pagesのサブディレクトリ配下で動作させるための設定
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
 });
