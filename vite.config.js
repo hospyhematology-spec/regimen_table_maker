29b57cb9a2c2d172ff88e0dd5d960e7b5214c87f
hospyhematology-spec <hospy.hematology@gmail.com>
2026-03-17 09:37:05 +0900

    Fix: final attempt at relative paths and clean config #13

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/regimen_table_maker/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
